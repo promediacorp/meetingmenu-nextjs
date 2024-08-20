3981c14deadd9d1bd69d0b5ccdb723657b9b0a33
avi wilensky <wilensky@gmail.com>
2024-08-20 15:20:02 -0400

    footer fix

--- components/ui/Footer/Footer.jsx
+++ components/ui/Footer/Footer.jsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 const Footer = () => {
     return (
         <footer className="mt-20">
@@ -5,15 +7,23 @@ const Footer = () => {
                 <div className="mt-10 py-8 border-t border-gray-800 items-center justify-between sm:flex">
                     <p className="text-gray-400 text-center">© 2024 MeetingMenu. All rights reserved.</p>
                     <div className="flex items-center justify-center gap-x-6 text-gray-500 mt-6 sm:mt-0">
-                        <a href="/privacy" className="hover:text-gray-200 duration-150">Privacy Policy</a>
-                        <a href="/support" className="hover:text-gray-200 duration-150">Support</a>
-                        <a href="/help" className="hover:text-gray-200 duration-150">Help</a>
-                        <a href="/terms" className="hover:text-gray-200 duration-150">Terms of Service</a>
+                        <Link href="/privacy" passHref>
+                            <a className="hover:text-gray-200 duration-150">Privacy Policy</a>
+                        </Link>
+                        <Link href="/support" passHref>
+                            <a className="hover:text-gray-200 duration-150">Support</a>
+                        </Link>
+                        <Link href="/help" passHref>
+                            <a className="hover:text-gray-200 duration-150">Help</a>
+                        </Link>
+                        <Link href="/terms" passHref>
+                            <a className="hover:text-gray-200 duration-150">Terms of Service</a>
+                        </Link>
                     </div>
                 </div>
             </div>
         </footer>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
